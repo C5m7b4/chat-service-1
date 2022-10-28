fbd9beb7e8656804da4990e53281166ef384508c
C5m7b4 <mbedingfield@dcrpos.com>
2022-10-27 20:49:20 -0500

    add connection

--- users-service/src/index.ts
+++ users-service/src/index.ts
@@ -1,1 +1,9 @@
+import 'reflect-metadata';
+
+import { initConnection } from '#root/db/connection';
+
+initConnection().then(() => {
+  console.log('DB connection established');
+});
+
 console.log('users_service says hi');
