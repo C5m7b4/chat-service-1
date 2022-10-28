6087635ef35e07d60aadf4440718a3201cbd14ce
C5m7b4 <mbedingfield@dcrpos.com>
2022-10-28 15:48:17 -0500

    add first migration

--- users-service/src/db/connection.ts
+++ users-service/src/db/connection.ts
@@ -1,10 +1,12 @@
 import config from 'config';
 import { createConnection, Connection } from 'typeorm';
+import User from './entities/User';
 
 let connection: Connection;
 
 export const initConnection = async () => {
   connection = await createConnection({
+    entities: [User],
     type: 'mysql',
     url: <string>config.get('USERS_SERVICE_DB_URL'),
   });
