03e8f338c9f509dd527b7a5e01e3ebc4afc4f6ed
C5m7b4 <mbedingfield@dcrpos.com>
2022-10-28 19:18:24 -0500

    add logout, delete session, and create new user

--- users-service/src/server/routes.ts
+++ users-service/src/server/routes.ts
@@ -1,11 +1,13 @@
 import config from 'config';
 import dayjs from 'dayjs';
 import { Express } from 'express';
+import omit from 'lodash.omit';
 import { getConnection, getRepository } from 'typeorm';
 
 import User from '#root/db/entities/User';
 import UserSession from '#root/db/entities/UserSession';
 import passwordCompareSync from '#root/helpers/passwordCompareSync';
+import hashPassword from '#root/helpers/hashPassword';
 import generateUUID from '#root/helpers/generateUUID';
 
 const USER_SESSION_EXPIRY_HOURS = <number>(
@@ -15,6 +17,7 @@ const USER_SESSION_EXPIRY_HOURS = <number>(
 const setupRoutes = (app: Express) => {
   const connection = getConnection();
   const userRepository = getRepository(User);
+  const userSessionRepository = getRepository(UserSession);
 
   app.post('/sessions', async (req, res, next) => {
     if (!req.body.username || !req.body.password) {
@@ -51,9 +54,65 @@ const setupRoutes = (app: Express) => {
         .createQueryBuilder()
         .insert()
         .into(UserSession)
-        .values(userSession);
+        .values(userSession)
+        .execute();
 
       return res.json(userSession);
+    } catch (error) {
+      return next(error);
+    }
+  });
+
+  app.delete('/sessions/:sessionId', async (req, res, next) => {
+    try {
+      const userSession = await userSessionRepository.findOne(
+        req.params.sessionId
+      );
+
+      if (!userSession) return next(new Error('invalid session id'));
+
+      await userSessionRepository.remove(userSession);
+
+      return res.end();
+    } catch (error) {
+      return next(error);
+    }
+  });
+
+  app.get('/sessions/:sessionId', async (req, res, next) => {
+    try {
+      const userSession = await userSessionRepository.findOne(
+        req.params.sessionId
+      );
+
+      if (!userSession) return next(new Error('invalid session id'));
+
+      return res.json(userSession);
+    } catch (error) {
+      return next(error);
+    }
+  });
+
+  app.post('/users', async (req, res, next) => {
+    try {
+      if (!req.body.username || !req.body.password) {
+        return next(new Error('Invalid body'));
+      }
+
+      const newUser = {
+        id: generateUUID(),
+        passwordHash: hashPassword(req.body.password),
+        username: req.body.username,
+      };
+
+      await connection
+        .createQueryBuilder()
+        .insert()
+        .into(User)
+        .values([newUser])
+        .execute();
+
+      return res.json(omit(newUser, ['passwordHash']));
     } catch (error) {
       return next(error);
     }
