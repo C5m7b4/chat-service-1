101012f093dee3fc1405f62a643e9d565d927ddb
C5m7b4 <mbedingfield@dcrpos.com>
2022-10-29 15:51:38 -0500

    add user info to the session output

--- api-gateway/src/adapters/UsersService.ts
+++ api-gateway/src/adapters/UsersService.ts
@@ -3,11 +3,35 @@ import got from 'got';
 
 const USERS_SERVICE_URI = <string>config.get('USERS_SERVICE_URI');
 
+export interface User {
+  createdAt: string;
+  id: string;
+  userId: string;
+}
+
+export interface UserSession {
+  createdAt: string;
+  expiresAt: string;
+  id: string;
+  userId: string;
+}
+
 export default class UsersService {
-  static async fetchUsersSession({ sessionId }: { sessionId: string }) {
+  static async fetchUser({ userId }: { userId: string }): Promise<User | null> {
+    const body = await got.get(`${USERS_SERVICE_URI}/users/${userId}`).json();
+    if (!body) return null;
+    return <User>body;
+  }
+
+  static async fetchUsersSession({
+    sessionId,
+  }: {
+    sessionId: string;
+  }): Promise<UserSession | null> {
     const body = await got
       .get(`${USERS_SERVICE_URI}/sessions/${sessionId}`)
       .json();
-    return body;
+    if (!body) return null;
+    return <UserSession>body;
   }
 }
